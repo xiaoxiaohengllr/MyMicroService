a5c32726cbe35262543e885c4afd20e86d34376a
xiaoxiaoheng <oncwnuOviw32OlUIkeIeV6pqguvA@git.weixin.qq.com>
2020-09-22 11:54:54 +0800

    MyMicroService.Tool.CodeGenerationTool

---
+++ MyMicroService.Tool.CodeGenerationTool/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyMicroService.Tool.CodeGenerationTool
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
